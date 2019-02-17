7f4b10fcbe0710246b3919a319f78b6b99c5ffc7
Ragnar Lonn <ragnar@lonn.org>
2019-02-17 20:04:42 +0100

    Working in insecure mode, failing SSL handshake in secure mode

--- test/src/Pushdata_ESP8266_SSL.h
+++ test/src/Pushdata_ESP8266_SSL.h
@@ -48,40 +48,110 @@ class Pushdata_ESP8266_SSL {
             WiFi.macAddress(mac);
             sprintf(tsname, "---%02x%02x%02x%02x%02x%02x__", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
             DBGPRINT("Sending to TS "); DBGPRINTLN(tsname);
-            return _send(tsname, value, NULL, 0);
+            ESP.wdtDisable();
+            int ret = _send(tsname, value, NULL, 0);
+            ESP.wdtEnable(5000);
+            return ret;
         }
         // Store data in a specific TS
         int send(const char *tsname, float value) {
-            return _send(tsname, value, NULL, 0);
+            ESP.wdtDisable();
+            int ret = _send(tsname, value, NULL, 0);
+            ESP.wdtEnable(5000);
+            return ret;
         }
         // Store data in a specific TS and with tags
         // **tags = { "key1", "val1", "key2", "val2" ... }
         int send(const char *tsname, float value, const char **tags, int numtags) {
-            return _send(tsname, value, tags, numtags);
+            ESP.wdtDisable();
+            int ret = _send(tsname, value, tags, numtags);
+            ESP.wdtEnable(5000);
+            return ret;
         }
         // Function that does the actual sending
         int _send(const char *tsname, float value, const char **tags, int numtags) {
-            static char pubkey[] = "-----BEGIN PUBLIC KEY-----"
-"MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAtbra1mhjjMaiZlIZIsky"
-"vewXvNuJNLE8RKZtstuRSLu5TEyIHwaeFb9oKe+bHDRVmi3a15qaedhlaTNNAx1Z"
-"o0pRg2c3yRgJr6XocO41jqGOP8WGw6008B9XjKpvL9NuEfpUHXeT6hlFsLk87av4"
-"S/MOTGMHnaXhOU8mRQAiw3vRTEY/zrx3hMFPGcFEmKcEWKlkLSnqFQwnNy/74iBg"
-"VQYN6MbtsmscaPB/luTVv1lX8+MBxXpQWwSSqKbDwUGZUEcE0S9lSQ416TlmTntx"
-"knFJTidG98mFIJ8ryZki1ZINY8NsIBan35JacNR8l6nS8enFPL3DpZMgKIIZCs0W"
-"AwIDAQAB"
-"-----END PUBLIC KEY-----";
+            const char pubkey[] = "-----BEGIN PUBLIC KEY-----\n"
+            "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAtbra1mhjjMaiZlIZIsky\n"
+            "vewXvNuJNLE8RKZtstuRSLu5TEyIHwaeFb9oKe+bHDRVmi3a15qaedhlaTNNAx1Z\n"
+            "o0pRg2c3yRgJr6XocO41jqGOP8WGw6008B9XjKpvL9NuEfpUHXeT6hlFsLk87av4\n"
+            "S/MOTGMHnaXhOU8mRQAiw3vRTEY/zrx3hMFPGcFEmKcEWKlkLSnqFQwnNy/74iBg\n"
+            "VQYN6MbtsmscaPB/luTVv1lX8+MBxXpQWwSSqKbDwUGZUEcE0S9lSQ416TlmTntx\n"
+            "knFJTidG98mFIJ8ryZki1ZINY8NsIBan35JacNR8l6nS8enFPL3DpZMgKIIZCs0W\n"
+            "AwIDAQAB\n"
+            "-----END PUBLIC KEY-----";
+            /*
+            const char LetsEncryptRootCert[] = "-----BEGIN CERTIFICATE-----\n"
+                "MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw\n"
+                "TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh\n"
+                "cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4\n"
+                "WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu\n"
+                "ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY\n"
+                "MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc\n"
+                "h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+\n"
+                "0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U\n"
+                "A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW\n"
+                "T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH\n"
+                "B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC\n"
+                "B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv\n"
+                "KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn\n"
+                "OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn\n"
+                "jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbw\n"
+                "qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI\n"
+                "rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV\n"
+                "HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkq\n"
+                "hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL\n"
+                "ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ\n"
+                "3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK\n"
+                "NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5\n"
+                "ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur\n"
+                "TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC\n"
+                "jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc\n"
+                "oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq\n"
+                "4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA\n"
+                "mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d\n"
+                "emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=\n"
+                "-----END CERTIFICATE-----\n";
+                */
             if ((email == NULL || strlen(email) == 0) && (apikey == NULL || strlen(apikey) == 0)) {
                 Serial.println("Pushdata_ESP8266_SSL: Error: you must set either an email or an api key");
                 return 0;
             }
+            DBGPRINTLN("Pushdata_ESP8266_SSL: BearSSL::WifiClientSecure init");
             BearSSL::WiFiClientSecure client;
-            BearSSL::PublicKey key(pubkey);
+            /*
+            BearSSL::X509List x509;
+            if (x509.append(LetsEncryptRootCert)) {
+                DBGPRINTLN("Pushdata_ESP8266_SSL: parsing of x509 root cert succeeded");
+            } else {
+                DBGPRINTLN("Pushdata_ESP8266_SSL: parsing of x509 root cert failed");
+            }
+            client.setTrustAnchors(&x509);
+            */
+            DBGPRINTLN("Pushdata_ESP8266_SSL: BearSSLPublicKey init");
+            BearSSL::PublicKey key;
+            if (key.parse((uint8_t *)pubkey, strlen(pubkey))) {
+                DBGPRINTLN("Pushdata_ESP8266_SSL: key.parse() succeeded");
+            } else {
+                DBGPRINTLN("Pushdata_ESP8266_SSL: key.parse() failed");
+                while (1);
+            }
+            DBGPRINTLN("Pushdata_ESP8266_SSL: client.setKnownKey()");
             client.setKnownKey(&key);
             //client.setInsecure();
+            yield();
+            client.setCiphersLessSecure();
+            DBGPRINTLN("Pushdata_ESP8266_SSL: client.connect()");
             if (!client.connect("pushdata.io", 443)) {
                 Serial.println("Pushdata_ESP8266: Error: failed to connect to pushdata.io:443");
+                char buf[200];
+                int err = client.getLastSSLError(buf, 199);
+                buf[199] = '\0';
+                Serial.println("Last SSL error was:");
+                Serial.println(buf);
+                Serial.print("ERRCODE: "); Serial.println(err);
                 return 0;
             }
+            DBGPRINTLN("Pushdata_ESP8266_SSL: Connected to pushdata.io:443");
             static char packetBuf[250];
             memset((void *)packetBuf, 0, 250);
             sprintf(packetBuf, "{\"name\":\"%s\",\"points\":[{\"value\":%f}]", tsname, value);
--- test/src/main.cpp
+++ test/src/main.cpp
@@ -3,10 +3,17 @@
 #include <U8g2lib.h>
 #include <ESP8266WiFi.h>
 #include "Pushdata_ESP8266_SSL.h"
+#include "user_interface.h"
 
 U8X8_SSD1306_128X32_UNIVISION_SW_I2C u8x8(/* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ 16);   // Adafruit Feather ESP8266/32u4 Boards + FeatherWing OLED
 
 Pushdata_ESP8266_SSL pd;
+
+//char ssid[] = "Goto 10";
+//char passwd[] = "datorbyxa";
+
+char ssid[] = "APA";
+char passwd[] = "deadbeef42";
 
 void OLEDOutput(const char *s1, const char *s2) {
   u8x8.clearLine(2);
@@ -20,6 +27,7 @@ void OLEDOutput(const char *s1, const char *s2) {
 }
 
 void setup() {
+  system_update_cpu_freq(160);
   delay(1000);
   Serial.begin(115200);
   Serial.println();
@@ -30,7 +38,7 @@ void setup() {
   OLEDOutput("Starting...", "");
   pd.setEmail("ragnar@lonn.org");
   pd.setApiKey("sq53rb8ozvdufcq2ayzq");
-  while (pd.connectWiFi("Goto 10", "datorbyxa") != WL_CONNECTED) {
+  while (pd.connectWiFi(ssid, passwd) != WL_CONNECTED) {
     OLEDOutput("Connecting...", "");
     delay(500);
     OLEDOutput("", "");
@@ -44,19 +52,23 @@ int pos = 0;
 
 void loop() {
   if (millis() % 5000 == 0) {
+    /*
     if (++pos > 2) {
       pos = 0;
     }
+    */
     char bufs[][16] = { "MAC            ", "Chip           ", "Flash          " };
     char buf[12];
     sprintf(bufs[1], "Chip  %s", itoa(ESP.getChipId(), buf, 10));
     sprintf(bufs[2], "Flash %s", itoa(ESP.getFlashChipId(), buf, 10));
+    /*
     WiFi.macAddress((uint8_t*)buf);
     sprintf(bufs[0], "HW %02x%02x%02x%02x%02x%02x", buf[0], buf[1], buf[2], buf[3], buf[4], buf[5]);
+    */
     if (pos == 0) 
-      OLEDOutput(bufs[0], bufs[1]);
-    else if (pos == 1)
       OLEDOutput(bufs[1], bufs[2]);
+    else if (pos == 1)
+      OLEDOutput(bufs[0], bufs[1]);
     else if (pos == 2)
       OLEDOutput(bufs[2], bufs[0]);
     pd.send((float)millis());
